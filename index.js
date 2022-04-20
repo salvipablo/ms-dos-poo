1470e1d919c89d482e436c2f1b1a9b809623adfb
psalvi <salvipablo@gmail.com>
2022-04-20 10:56:18 -0300

    pass project to typescript
    Resolves: F#0002

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
+"use strict";
 /* Variables */
 let comando;
-
 process.stdout.write("C:\\");
-
-process.stdin.on('data', function(data){
-  comando = data.toString();
-
-  if (comando.trim().toUpperCase() == "EXIT") {
-    process.exit();
-  }
-  
-  process.stdout.write("C:\\");
+process.stdin.on('data', function (data) {
+    comando = data.toString();
+    if (comando.trim().toUpperCase() == "EXIT") {
+        process.exit();
+    }
+    process.stdout.write("C:\\");
 });
